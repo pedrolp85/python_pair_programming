4343bf32d5b450f225b93f8947787ec89d4a37e1
Pedro Lopez <>
2023-05-19 16:56:05 +0200

    initial commit

---
+++ solution.py
@@ -0,0 +1,52 @@
+from typing import List
+from utilities import timeit
+
+@timeit
+def solve_ranked_pythonic(ranked, player):
+    player_rank = []
+    unique_sorted_rank = list(set(ranked))
+    unique_sorted_rank.sort()
+
+    i = 0
+    current_position = len(unique_sorted_rank) + 1
+    for score in player:
+        if current_position > 1:
+            while i < len(unique_sorted_rank) and score >= unique_sorted_rank[i]:
+                current_position -= 1
+                i += 1
+
+        player_rank.append(current_position)
+
+    return player_rank
+
+
+@timeit
+def solve_ranked_efficient(ranking, player):
+    player_rank = []
+    player_index = len(player) - 1
+    ranking_size = len(ranking)
+    ranking_index = 0
+    position = 1
+
+    while ranking_index < ranking_size and player_index >= 0:
+        current_player = player[player_index]
+        ranking_points = ranking[ranking_index]
+        if current_player >= ranking_points:
+            player_rank.append(position)
+            player_index -= 1
+        else:
+            ranking_index += 1
+            if ranking_index < ranking_size and ranking[ranking_index] < ranking[ranking_index - 1]:
+                position += 1
+    position += 1
+    while player_index >= 0:
+        player_rank.append(position)
+        player_index -= 1
+
+    return player_rank[::-1]
+
+
+
+
+def test_ranked_pythonic
+
