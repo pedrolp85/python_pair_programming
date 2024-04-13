9c1fdcb62dc391e3d793ccb642e1d7e9617f62e8
Pedro Lopez <pedrolopz.85@gmail.com>
2024-04-13 13:04:58 +0200

    kata worklads

--- README.md
+++ README.md
@@ -123,6 +123,81 @@ Example
         [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
         [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
+
+## read workloads
+
+Sean los fichero estáticos .yml en static_files/OCPClusterX.yml representaciones de workloads 
+de K8s
+
+Lee los ficheros en la estructura que prefieras y hay que satisfacer los tests en read_workloads.py
+
+Nota:
+
+    Cada cluster puede tener un número indeterminado de nodos
+    Cada nodo es único, y suma para la adición de recursos
+    Un nodo puede tener o no la línea node_labels
+
+    Los namespaces son estructuras lógicas que se replican en todos los nodos de k8s
+    Su utilidad es delimitar el acceso a recursos de las aplicaciones
+    En los ficheros de test cada ns tiene un nombre diferente
+
+    Al similar ocurre con los pods, 2 pods con el mismo nombre pueden existir en ns diferentes
+
+    Labels:
+
+        Las etiquetas se usan para seleccionar recursos en kubernetes
+        Sin embargo, son estructuras del tipo clave:valor, con sus limitaciones
+
+        No podemos tener 2 valores diferentes para una misma clave dentro de un mismo dominio 
+        de colisión
+
+        Para ello se establece una precedencia en sentido descendente
+
+        Consideramos "iguales" 2 claves que compartan cluster, nodo, ns o pod
+        Viendo el ejemplo del cluster 1:
+
+        Para el Pod: pod_1
+        label_tier: pod1
+
+        Para el Pod: pod_2
+        label_tier: pod_2
+
+        Las etiquetas del cluster deben contener ambos valores para esa clave
+        Los tests siempre nos pedirán los valores finales, con la precedencia aplicada
+
+        Almacenar los valores intermedios no es obligatorio
+
+
+
+    test_get_cluster_name(input_file, expected_cluster_name)
+        devuelve el cluster_name
+    
+    test_get_cpu_count(input_file, expected_cpu_count)
+        devuelve la suma de las cpu de los nodos que forman el cluster
+
+    test_get_mem_count(input_file, expected_mem_count)
+        devuelve la suma de GB de los nodos que forman el cluster
+
+    test_get_all_nodes()
+        devuelve todos los nodos del cluster
+
+    test_get_all_ns()
+        devuelve todos los ns del cluster
+
+    test_get_all_pods()
+        devuelve todos los pods del cluster        
+
+    test_get_ns_by_node()
+        devuelve los ns de los nodos que macheen con la regex 
+
+    test_get_pods_by_ns()
+        devuelve los ns de los nodos que macheen con la regex 
+
+
+
+
+
+
 ## problem name
 
 a link to the problem
---
+++ read_workloads.py
@@ -0,0 +1,177 @@
+import pytest
+import yaml
+
+
+def read_yaml_file(file_name):
+    with open(file_name) as cluster_file:
+        return cluster_file.read()
+
+
+def parse_yaml_file(yaml_string):
+    return yaml.safe_load(yaml_string)
+
+
+def dict_to_cluster(parsed_yaml):
+    return None
+
+
+@pytest.mark.parametrize(
+    "input_file, expected_cluster_name",
+    [
+        ("static_files/OCPCluster1.yml", "OCPCluster1"),
+        ("static_files/OCPCluster2.yml", "OCPCluster2"),
+        ("static_files/OCPCluster3.yml", "OCPCluster3"),
+        ("static_files/OCPCluster4.yml", "OCPCluster4"),
+        ("static_files/OCPCluster5.yml", "OCPCluster5"),
+    ],
+    ids=["Cluster1", "Cluster2", "Cluster3", "Cluster4", "Cluster5"],
+)
+def test_get_cluster_name(input_file, expected_cluster_name):
+    read_yaml = read_yaml_file(input_file)
+    parsed_file = parse_yaml_file(read_yaml)
+
+    cluster = dict_to_cluster(parsed_file)
+
+    cluster_name = None
+
+    assert cluster_name == expected_cluster_name
+
+
+@pytest.mark.parametrize(
+    "input_file, expected_cpu_count",
+    [
+        ("static_files/OCPCluster1.yml", 3.0),
+        ("static_files/OCPCluster2.yml", 5.0),
+        ("static_files/OCPCluster3.yml", 11.0),
+        ("static_files/OCPCluster4.yml", 14.0),
+        ("static_files/OCPCluster5.yml", 8.0),
+    ],
+    ids=["Cluster1", "Cluster2", "Cluster3", "Cluster4", "Cluster5"],
+)
+def test_get_cpu_count(input_file, expected_cpu_count):
+    read_yaml = read_yaml_file(input_file)
+    parsed_file = parse_yaml_file(read_yaml)
+
+    cluster = dict_to_cluster(parsed_file)
+    cpu_cores = None
+
+    assert cpu_cores == expected_cpu_count, "Unmatched CPU count"
+
+
+@pytest.mark.parametrize(
+    "input_file, expected_memory_gig",
+    [
+        ("static_files/OCPCluster1.yml", 12.0),
+        ("static_files/OCPCluster2.yml", 6.0),
+        ("static_files/OCPCluster3.yml", 20.0),
+        ("static_files/OCPCluster4.yml", 16.0),
+        ("static_files/OCPCluster5.yml", 2.0),
+    ],
+    ids=["Cluster1", "Cluster2", "Cluster3", "Cluster4", "Cluster5"],
+)
+def test_get_mem_count(input_file, expected_memory_gig):
+    read_yaml = read_yaml_file(input_file)
+    parsed_file = parse_yaml_file(read_yaml)
+
+    cluster = dict_to_cluster(parsed_file["data"][0]["OCPCluster"])
+
+    memory_gig = None
+
+    assert memory_gig == expected_memory_gig, "Unmatched Memory count"
+
+
+@pytest.mark.parametrize(
+    "input_file, expected_nodes",
+    [
+        ("static_files/OCPCluster1.yml", ["node1", "node2"]),
+        ("static_files/OCPCluster2.yml", ["node1B"]),
+        ("static_files/OCPCluster3.yml", ["node1C", "node2C"]),
+        ("static_files/OCPCluster4.yml", ["node1D", "node2D"]),
+        ("static_files/OCPCluster5.yml", ["node1E", "node2E"]),
+    ],
+    ids=["Cluster1", "Cluster2", "Cluster3", "Cluster4", "Cluster5"],
+)
+def test_get_all_nodes(input_file, expected_nodes):
+    read_yaml = read_yaml_file(input_file)
+    parsed_file = parse_yaml_file(read_yaml)
+
+    cluster = dict_to_cluster(parsed_file["data"][0]["OCPCluster"])
+    nodes = None
+
+    assert sorted(nodes) == sorted(expected_nodes), "Unmatched node names"
+
+
+@pytest.mark.parametrize(
+    "input_file, expected_namespaces",
+    [
+        ("static_files/OCPCluster1.yml", ["ns1", "ns2", "ns3"]),
+        ("static_files/OCPCluster2.yml", ["ns1"]),
+        ("static_files/OCPCluster3.yml", ["ns1", "ns2", "ns3", "ns4", "ns5"]),
+        ("static_files/OCPCluster4.yml", ["ns1", "ns2"]),
+        ("static_files/OCPCluster5.yml", ["ns1", "ns2", "ns3"]),
+    ],
+    ids=["Cluster1", "Cluster2", "Cluster3", "Cluster4", "Cluster5"],
+)
+def test_get_all_namespaces(input_file, expected_namespaces):
+    read_yaml = read_yaml_file(input_file)
+    parsed_file = parse_yaml_file(read_yaml)
+
+    cluster = dict_to_cluster(parsed_file["data"][0]["OCPCluster"])
+    namespaces = None
+
+    assert sorted(namespaces) == sorted(expected_namespaces), "Unmatched ns names"
+
+
+@pytest.mark.parametrize(
+    "input_file, expected_pods",
+    [
+        ("static_files/OCPCluster1.yml", ["pod1", "pod2", "pod3"]),
+        ("static_files/OCPCluster2.yml", ["pod1", "pod2"]),
+        (
+            "static_files/OCPCluster3.yml",
+            [
+                "pod1",
+                "pod2",
+                "pod3",
+                "pod4",
+                "pod2",
+                "pod1",
+                "pod2",
+                "pod4",
+                "pod1",
+                "pod4",
+                "pod2",
+            ],
+        ),
+        ("static_files/OCPCluster4.yml", ["pod1", "pod2"]),
+        ("static_files/OCPCluster5.yml", ["pod1", "pod2", "pod3"]),
+    ],
+    ids=["Cluster1", "Cluster2", "Cluster3", "Cluster4", "Cluster5"],
+)
+def test_get_all_pods(input_file, expected_pods):
+    read_yaml = read_yaml_file(input_file)
+    parsed_file = parse_yaml_file(read_yaml)
+
+    cluster = dict_to_cluster(parsed_file["data"][0]["OCPCluster"])
+    pods = None
+
+    assert sorted(pods) == sorted(expected_pods), "Unmatched pod names"
+
+
+@pytest.mark.parametrize(
+    "input_file, nodes_regex,expected_ns",
+    [
+        ("static_files/OCPCluster1.yml", "node1", ["ns1", "ns2"]),
+        ("static_files/OCPCluster1.yml", "node2", ["ns3"]),
+        ("static_files/OCPCluster1.yml", "node", ["ns1", "ns2", "ns3"]),
+        ("static_files/OCPCluster1.yml", "fake:regex", []),
+    ],
+    ids=["node1", "node2", "node", "fake_regex"],
+)
+def test_get_ns_by_node(input_file, nodes_regex, expected_ns):
+    read_yaml = read_yaml_file(input_file)
+    parsed_file = parse_yaml_file(read_yaml)
+
+    cluster = dict_to_cluster(parsed_file["data"][0]["OCPCluster"])
+    ns = None
+    assert sorted(ns) == sorted(expected_ns), "Unmatched filtered ns names"
